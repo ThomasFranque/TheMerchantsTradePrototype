571ab00b60dd33fd6cc0c2275c9147c5712d127d
Afonso Rosa <a21802169@alunos.ulht.pt>
2019-12-16 12:00:51 +0000

    Code fixed by thomas, build in the project folder

---
+++ TheMerchantsTrade/Assets/Prefabs/Menus/Trading Menu/MainCame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainCame : MonoBehaviour
+{
+    private Canvas canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = GetComponent<Canvas>();
+        canvas.worldCamera = UnityEngine.Camera.main;
+        canvas.planeDistance = 3.0f;
+    }
+}
